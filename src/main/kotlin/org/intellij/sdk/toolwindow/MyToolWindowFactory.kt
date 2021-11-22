699cb4056772105e27c6e1904d99c70ba656a0a3
Chris Menezes <chris@menezes.ca>
2021-11-21 22:49:24 -0500

    Add new UI components
    and custom cell renderer

--- src/main/kotlin/org/intellij/sdk/toolwindow/MyToolWindowFactory.kt
+++ src/main/kotlin/org/intellij/sdk/toolwindow/MyToolWindowFactory.kt
@@ -1,9 +1,7 @@
 package org.intellij.sdk.toolwindow
 
+import com.github.cmnzs.metaj.services.*
 import com.intellij.openapi.diagnostic.Logger
-import com.github.cmnzs.metaj.services.GardenItem
-import com.github.cmnzs.metaj.services.IdeaGardenState
-import com.github.cmnzs.metaj.services.MyApplicationService
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
@@ -13,10 +11,8 @@ import com.intellij.ui.components.JBList
 import com.intellij.ui.layout.panel
 import com.intellij.uiDesigner.core.GridConstraints
 import com.intellij.uiDesigner.core.GridLayoutManager
-import org.intellij.lang.annotations.JdkConstants
-import java.awt.Color
+import com.jetbrains.rd.swing.textProperty
 import java.awt.Component
-import java.awt.FlowLayout
 import javax.swing.*
 
 
@@ -31,10 +27,6 @@ class MyToolWindowFactory : ToolWindowFactory, DumbAware {
      * @param toolWindow current tool window
      */
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-//        val contentFactory = ContentFactory.SERVICE.getInstance()
-//        val content = contentFactory.createContent(myToolWindow.myToolWindowContent, "", false)
-//        val content = contentFactory.createContent(createDialogPanel(), "Hello World", false)
         val content = toolWindow.contentManager.factory.createContent(IdeaGarden().createDialogPanel(), null, false)
         toolWindow.contentManager.addContent(content)
     }
@@ -43,7 +35,8 @@ class MyToolWindowFactory : ToolWindowFactory, DumbAware {
 val LOG = Logger.getInstance("MetaJUI")
 
 class IdeaGarden {
-    val listModel = DefaultListModel<String>()
+    private val goalListModel = DefaultListModel<GardenItem>()
+    private val actionListModel = DefaultListModel<GardenItemAction>()
 
     companion object {
         fun service(): MyApplicationService {
@@ -53,62 +46,95 @@ class IdeaGarden {
     init {
 
         if (service().myState.items.isEmpty()) {
-            val list = IdeaGardenState.items.map { it.name }
-            listModel.addAll(list)
+            val list = IdeaGardenState.items
+            goalListModel.addAll(list)
             updateState(list)
         } else {
-            val list = service().myState.items.map { it.name }
-            listModel.addAll(list)
+            val list = service().myState.items
+            goalListModel.addAll(list)
         }
     }
 
-    fun updateState(data: List<String> ) {
-        service().myState = service().myState.copy(items = data.map { GardenItem(it) })
-
+    private fun updateState(data: List<GardenItem> ) {
+        service().myState = service().myState.copy(items = data)
     }
 
     fun createDialogPanel(): DialogPanel = panel {
 
         row("""Welcome to the IDEA Garden"""){}
         row{
-            createMainPanel(listModel)()
+            createMainPanel()()
         }
     }
 
-    fun createMainPanel(goalListModel: DefaultListModel<String>): JComponent {
+    fun createMainPanel(): JComponent {
 
         val topPanel = JPanel(GridLayoutManager(1,2))
 
         val goalsPanel = JPanel(GridLayoutManager(4,1))
-
-
         val goalsLabel = JLabel("Goals")
         val addGoalButton = JButton("Add Goal")
         val goalEditor = JEditorPane()
 
         val goalList = JBList(goalListModel)
+
         val customRenderer = LabelRenderer()
         goalList.cellRenderer = customRenderer
+        goalList.selectionMode = ListSelectionModel.SINGLE_SELECTION
+
+        goalList.addListSelectionListener { e ->
+            if (!e.valueIsAdjusting) {
+                val gardenItem = service().myState.items.elementAt(goalList.selectedIndex)
+                actionListModel.removeAllElements()
+                actionListModel.addAll(gardenItem.actions)
+            }
+        }
 
         addGoalButton.addActionListener {
             val newItem = GardenItem(goalEditor.text)
-            listModel.add(listModel.size, newItem.name)
-            val l = listModel.elements().toList()
+            goalListModel.add(goalListModel.size, newItem)
+            val l = goalListModel.elements().toList()
             updateState(l)
         }
 
+
+        goalEditor.text = "Enter new goal here..."
+        goalEditor.background = goalEditor.background.darker().darker()
+
         goalsPanel.add(goalsLabel, glh(0,0))
         goalsPanel.add(goalList, glh(1, 0))
-        goalsPanel.add(addGoalButton, glh(2, 0))
+        goalsPanel.add(goalEditor, glh(2, 0))
+        goalsPanel.add(addGoalButton, glh(3, 0))
 
-        val actionsPanel = JPanel(GridLayoutManager(2,1))
+        // Actions panel
+        val actionsPanel = JPanel(GridLayoutManager(4,1))
         val actionsLabel = JLabel("Actions")
         val addActionButton = JButton("Add Action")
-        addActionButton.addActionListener { LOG.info("Action event listen : $it") }
+        val actionEditor = JEditorPane()
+
+        actionEditor.text = "Enter new action..."
+        actionEditor.background = goalEditor.background.darker().darker()
+
+        val actionList = JBList(actionListModel)
+        actionList.cellRenderer = customRenderer
+
+        addActionButton.addActionListener {
+            val i = goalList.selectedIndex
+            val gardenItem = goalListModel[i]
+            val newAction = GardenItemAction(actionEditor.text, ActionStatus.NEW)
+            val newActions = gardenItem.actions + newAction
+
+            actionListModel.add(actionListModel.size, newAction)
+            val newItem = gardenItem.copy(actions = newActions)
+            goalListModel[i] = newItem
+            val l = goalListModel.elements().toList()
+            updateState(l)
+        }
 
         actionsPanel.add(actionsLabel, glh(0, 0))
-
-        actionsPanel.add(addActionButton, glh(1, 0))
+        actionsPanel.add(actionList, glh(1, 0))
+        actionsPanel.add(actionEditor, glh(2, 0))
+        actionsPanel.add(addActionButton, glh(3, 0))
 
         topPanel.add(goalsPanel, glh(0,0))
         topPanel.add(actionsPanel, glh(0, 1))
@@ -125,7 +151,13 @@ class LabelRenderer: DefaultListCellRenderer() {
         isSelected: Boolean,
         cellHasFocus: Boolean
     ): Component {
-        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+        if (value is GardenItem) {
+            super.getListCellRendererComponent(list, "[${value.status}] ${value.name}", index, isSelected, cellHasFocus)
+        } else if (value is GardenItemAction) {
+            super.getListCellRendererComponent(list, "[${value.status}] ${value.description}", index, isSelected, cellHasFocus)
+        } else {
+            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+        }
 
         if (index % 2 == 0) {
             this.background = this.background.darker()
@@ -138,8 +170,6 @@ fun gridLayoutHelper(row: Int, col: Int): GridConstraints {
     val x = GridConstraints()
     x.row = row
     x.column = col
-    x.vSizePolicy = GridConstraints.SIZEPOLICY_WANT_GROW
-    x.hSizePolicy = GridConstraints.SIZEPOLICY_WANT_GROW
     return x
 }
 fun glh(row: Int, col: Int): GridConstraints {
